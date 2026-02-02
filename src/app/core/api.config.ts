a77277a26c9921d5eec287915bc66f4b8b3154c0
ASV47 <ahmedmohammedmahermakled@gmail.com>
2026-02-02 13:41:58 +0200

    hhggdds

--- src/app/core/api.config.ts
+++ src/app/core/api.config.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'https://YOUR-BACKEND.up.railway.app';
+export const API_BASE_URL = 'https://backend-production-4e83c.up.railway.app/';
 // If you use proxy during dev, you can set: export const API_BASE_URL = '';
